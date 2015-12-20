9222fe12ef1702bc5dc2061ea0fe5485d21fb2f6
liamlevesque <liam.levesque@feralcatlabs.com>
2015-12-19 20:22:09 -0800

    getting up and running

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,43 @@
 $(function(){
 
+
 	$.ajax({
 		method: "GET",
-		url: "https://www.rbauction.com/rba-api/journal-content?articleId=INSPECTION_8699598",
-		dataType: "json"
-	})
-	.done(function( content ) {
-		console.log("success");
-		console.log( content );
-	})
-	.fail(function( jqXHR, textStatus ) {
-		console.log(jqXHR.responseText);
-		console.log("Request failed: " + textStatus);
-	}); 
+		url: "assets/js/data/lotdata.json",
+		dataType: "json", 
+		success: function(data){
+			buildLotTable( data );
+			buildCurrentLot(data);
+		},
+		error: function(jqXHR, textStatus){
+			console.log(jqXHR.responseText);
+			console.log("Request failed: " + textStatus);
+		}
+	});
 
 });
+
+
+function buildCurrentLot(data){
+	var currentLot = data[0].lots[$('.js--lot-info').data('lot')],
+		template2 = $('#currentlot').html();
+
+	var rendered = Mustache.render(template2, currentLot);
+	$('.js--lot-info').html(rendered);	
+
+}
+
+
+function buildLotTable(data){
+
+	var lots = data[0].lots,
+		template = $('#lot').html();
+		
+	Mustache.parse(template);   // optional, speeds up future uses
+	
+	for (var i = 0; i < lots.length; i++) {
+        var rendered = Mustache.render(template, lots[i]);
+		$('.js--lots-all').append(rendered);			
+    }
+	
+}
