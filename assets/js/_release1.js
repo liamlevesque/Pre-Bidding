2b32a0a3f25fc8f0b06ecdfe68af06aa93317b2b
liamlevesque <liam.levesque@feralcatlabs.com>
2016-03-23 13:18:47 -0700

    more mobile testing

--- assets/js/_release1.js
+++ assets/js/_release1.js
@@ -58,8 +58,39 @@ function unloadMaxBidTooltip(target){
 			$('.js--max-bid-tooltip.tooltipstered').tooltipster('destroy');
 	    	unloadMaxBidTooltip();
 	    },
+	    onMaxBidInput: function(e, model){
+	    	console.log('hat');
+			switch(e.which) {
+		    	case 9:
+		    	case 13: // enter
+			        e.preventDefault();
+			        return true;
+			        break;
+
+		        case 38: // up
+		        	maxbidController.incrementMaxBid(100);
+		        	e.preventDefault();
+			       	return true;
+			        break;
+
+		        case 40: // down
+		        	maxbidController.incrementMaxBid(-100);
+		        	e.preventDefault();
+			       	return true;
+			       	break;
+
+		        default: 
+
+		        	if(e.which != 46 && e.which != 190 && e.which != 188 && e.which > 31 && (e.which < 48 || e.which > 57)) return false;
+		        	else return true; // exit this handler for other keys
+		    }
+		    e.preventDefault();
+		},
 	    onMaxBidBlur: function(e,model){
 	    	console.log(maxbidObject.maxbidAmount);
+	    },
+	    incrementMaxBid: function(amt){
+	    	console.log(amt);
 	    }
 
 	};
