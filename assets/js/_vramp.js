132aa9f8d0f4e41130f23c5873296a53047cd7d2
liamlevesque <liam.levesque@feralcatlabs.com>
2016-10-11 09:03:44 -0700

    updating vramp prototype

--- assets/js/_vramp.js
+++ assets/js/_vramp.js
@@ -36,42 +36,23 @@ var allConversions = [
 		
 
 	],
+	allLots,
 	sampleAuctioneerMessage = "Title In transit. Item must remain in yard until received.";
-
-
 
 
 var vrampObject = {
 		"auctionCCY": "CAD",
-		"conversions": [
-			],
+		"currentLot": 0,
+		"lotDetail":{},
+		"conversions": [],
 		"price" : 10000000,
 		"highBid" : 'Internet, SK, CAN',
 		"bidder" : '10005',
-		"isgroup":false,
 		"auctioneerMessage": null,
-		"choiceGroup": [
-			{
-				"lot":"2741",
-				"status": null
-			},
-			{
-				"lot":"2742",
-				"status": "sold"
-			},
-			{
-				"lot":"2743",
-				"status": null
-			},
-			{
-				"lot":"2743A",
-				"status": null
-			}
-
-		],
-		"otherRings": [
-			
-		]
+		"choiceSize":0,
+		"choiceGroup": null,
+		"activeChoiceLot": 0,
+		"otherRings": []
 	},
 
 	vrampController = {
@@ -109,6 +90,19 @@ var vrampObject = {
 			//TOGGLE WHICH BIT OF THE OTHER RINGS AREA IS VISIBLE
 			if(vrampObject.otherRings.length === 1) $(".js--vramp-bidding").addClass('s-other-rings_single');
 			else $(".js--vramp-bidding").addClass('s-other-rings_all');
+		},
+
+		nextLot: function(e,model){
+			navigateLot(1);
+		},
+
+		prevLot: function(e,model){
+			navigateLot(-1);
+		},
+
+		sellLot: function(e,model){
+			vrampObject.lotDetail.status = "sold";
+			if(isChoiceGroup()) updateChoiceGroup();
 		}
 
 	};
@@ -122,6 +116,10 @@ rivets.formatters.convertedVrampPrice = function(value, rate, ccy){
 	return formatprice(convertedVal.toFixed(0));
 }
 
+rivets.formatters.length = function (value) {
+  return value? value.length : 0;
+};
+
 rivets.binders.addccyclass = function(el, value){
 	$(el).addClass(value);
 }
@@ -130,3 +128,74 @@ rivets.bind($('.js--vramp-bidding'),{
 	vrampObject: vrampObject,
 	vrampController: vrampController
 });
+
+
+
+//LOAD LOT DATA
+
+	$.ajax({
+		method: "GET",
+		url: "assets/js/vramplots/data.json",
+		dataType: "json", 
+		success: function(data){
+			buildVrampTable( data );
+		},
+		error: function(jqXHR, textStatus){
+			console.log(jqXHR.responseText);
+			console.log("Request failed: " + textStatus);
+		}
+	});
+
+
+	function buildVrampTable(data){
+		
+		allLots = data[0].allOtherLots;
+		
+		navigateLot(0);
+ 
+	}
+
+	function navigateLot(increment){
+
+		vrampObject.currentLot += increment;
+
+		//CHECK IF THIS IS A CHOICE GROUP AND BUILD IF SO
+		if(isChoiceGroup()){
+			vrampObject.choiceGroup = allLots[vrampObject.currentLot];
+			updateChoiceGroup();
+		}
+
+		else{
+			vrampObject.choiceGroup = null;
+			vrampObject.lotDetail = allLots[vrampObject.currentLot];
+		}
+
+	}
+
+	function activateFirstAvailableChoiceLot(){
+		for(var i = 0; i < vrampObject.choiceGroup.length ; i++){
+			if(vrampObject.choiceGroup[i].status != "sold"){
+				vrampObject.choiceGroup[i].active = true;
+				return i;
+			}else{
+				vrampObject.choiceGroup[i].active = false;
+			}
+		}
+		//IF ALL ARE SOLD, STAY ON THE LAST ONE
+		return vrampObject.choiceGroup.length - 1;
+	} 
+
+	function isChoiceGroup(){
+		return Array.isArray(allLots[vrampObject.currentLot]);
+	}
+
+	function updateChoiceGroup(){
+		vrampObject.activeChoiceLot = activateFirstAvailableChoiceLot();
+		vrampObject.lotDetail = vrampObject.choiceGroup[vrampObject.activeChoiceLot];
+	}
+
+
+
+
+
+
